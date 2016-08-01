10ae4bedbadf917e74fae17461a5fdd189927eb6
Robert Whitney <xnite@xnite.me>
2016-08-01 02:40:34 -0500

    update install script

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-cp dosmon.conf /etc/dosmon.conf
+cp -r dosmon /etc/dosmon
 cp dosmon.pl /usr/sbin/dosmon.pl
 chmod +x /usr/sbin/dosmon.pl
-#cpan Net::Server::Daemonize
 mkdir /var/log/dosmon
 echo "Add the following line into crontab for the root user to start dosmon after boot:"
 echo "@reboot /usr/sbin/dosmon.pl start"
 echo "---"
-echo "Don't forget to configure dosmon by editing the configuration file at `/etc/dosmon.conf`"
-echo "Before you can use dosmon you will also need to make sure that tcpdump is installed on your system."
+echo "Don't forget to configure DOSMon by editing the configuration files in `/etc/dosmon/`"
+echo "Before you can use DOSMon you will also need to make sure that tcpdump is installed on your system."
 echo "Be sure to install perl dependencies for this program `cpan Net::Server::Damonize`"
 echo "Start dosmon by typing `/usr/sbin/dosmon.pl start`"
